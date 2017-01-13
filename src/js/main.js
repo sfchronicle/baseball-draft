1b57eac169e45b54dd531fe290bcffcc009f84a2
emma <emmaruthoneill@gmail.com>
2017-01-13 13:45:30 -0800

    working on js

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,18 @@
 require("./lib/social"); //Do not delete
 
-
+["slusser","schulman","shea","killion","jenkins","kroichick"].forEach(function(writer){
+  // set up clicking to update map interactive on mobile
+  document.getElementById("button"+writer).addEventListener("click", function() {
+    console.log(writer);
+    console.log(ballots);
+    console.log(ballots[writer]);
+    // var sectionElements = document.getElementsByClassName("section");
+    var buttonElements = document.getElementsByClassName("button");
+    for(var i = 0; i < buttonElements.length; i++){
+      // sectionElements[i].className = "section";
+      buttonElements[i].className = "button";
+    };
+    // document.getElementById("section"+section).className += " selected";
+    document.getElementById("button"+writer).className += " selected";
+  });
+});
